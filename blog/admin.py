c5bb44eed270dd90a3e76e5641727179bd0062a7
Mayar Abdelbasser <mayarabdelbasser539@gmail.com>
2026-06-28 12:20:01 +0300

    Add Author model

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,22 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Author
 
 
 # Register your models here.
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
-    list_display = ("title", "date")
+    list_display = (
+        "title",
+        "date",
+    )
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = (
+        Author.full_name,
+        "email_address",
+    )
 
 
 admin.site.register(Post, PostAdmin)
+admin.site.register(Author, AuthorAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,30 @@
 from django.db import models
 from django.urls import reverse
+from django.core.validators import RegexValidator
 
 
 # Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email_address = models.EmailField(
+        max_length=254,
+        validators=[
+            RegexValidator(
+                regex=r"((?!\.)[\w\-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$",
+                message="Enter a valid email",
+                code="invalid-email",
+            )
+        ],
+    )
+
+    def full_name(self):
+        return f"{self.first_name.capitalize()} {self.last_name.capitalize()}"
+
+    def __str__(self):
+        return self.full_name()
+
+
 class Post(models.Model):
     title = models.CharField(max_length=250, null=True)
     excerpt = models.CharField(max_length=250)
