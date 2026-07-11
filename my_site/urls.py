2cdf57a02885410f49a3e7fd34a2f35396beb49c
Mayar Abdelbasser <mayarabdelbasser539@gmail.com>
2026-07-11 18:41:27 +0300

    Add authentication UI

--- my_site/urls.py
+++ my_site/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("blog.urls")),
     path("api/", include("api.urls")),
+    path("auth/", include("authentication.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
